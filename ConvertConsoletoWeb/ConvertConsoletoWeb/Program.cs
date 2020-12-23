48ef6cec1e0912c89f12495e967be3d0548b2193
Ls7 <Ls7@DESKTOP-8R02R0E>
2020-12-23 18:39:50 +0800

    Convert the Console application to the Web application

---
+++ ConvertConsoletoWeb/ConvertConsoletoWeb/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConvertConsoletoWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreatHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreatHostBuilder(string[] args) =>
+            
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            { 
+                webBuilder.UseStartup<Startup>();
+            });
+            
+    }
+}
---
+++ ConvertConsoletoWeb/ConvertConsoletoWeb/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvertConsoletoWeb
+{
+    public class Startup
+    {
+        public void ConfigurationServices(IServiceCollection services)
+        { 
+        
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Convert Console application to Web Application");
+                });
+            });
+        }
+    }
+}
